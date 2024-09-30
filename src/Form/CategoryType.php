348cc3d4fc45d2bfcb19c233166e0ad4ce69ce4b
Sundermann <gasparsundermann@gmail.com>
2024-09-30 11:29:03 +0200

    Formulaire rapide

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,37 +3,26 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use App\Entity\Product;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ProductType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
             ->add('description')
-            ->add('price')
             ->add('image')
             ->add('date_add')
-            ->add('category', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'name',
-                'placeholder' => 'choisir une categorie',
-                'label' => 'Catégorie'
-            ])
-            ->add('save', SubmitType::class);
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Product::class,
+            'data_class' => Category::class,
         ]);
     }
 }
